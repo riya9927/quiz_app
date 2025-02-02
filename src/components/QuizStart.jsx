6abfeeec6a326e162aac0c8fd7fa60e73b2e825d
riya9927 <114976968+riya9927@users.noreply.github.com>
2025-02-02 09:29:06 +0530

    Update QuizStart.jsx

--- src/components/QuizStart.jsx
+++ src/components/QuizStart.jsx
@@ -22,8 +22,9 @@
 // }
 
 import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card'
+import { Button } from '../components/ui/button'
+import { Progress } from '../components/ui/progress'
 
 const QuizStart = ({ onStart }) => {
   return (
