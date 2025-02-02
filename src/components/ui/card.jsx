93ca5dba2a00b0832af6b1d32b32c369da53d39e
riya9927 <114976968+riya9927@users.noreply.github.com>
2025-02-02 10:36:06 +0530

    Update card.jsx

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -26,7 +26,6 @@ const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
   />
 ))
 CardTitle.displayName = "CardTitle"
-
 const CardContent = React.forwardRef(({ className, ...props }, ref) => (
   <div ref={ref} className={`p-6 pt-0 ${className}`} {...props} />
 ))
